1848b201b7b558e9cd89d50174524ea3789da053
xinxliu <lx1996@outlook.com>
2016-04-14 16:22:12 +0800

    test

---
+++ merge-sort/xmerge_sort.py
@@ -0,0 +1,33 @@
+
+def merge_sort(seq):
+    if len(seq) <= 1:
+        return seq
+    midIdx = len(seq)//2
+    leftSeq=merge_sort(seq[:midIdx])
+    rightSeq = merge_sort(seq[midIdx:])
+    return merge(leftSeq,rightSeq)
+
+def merge(lSeq,rSeq):
+    result = []
+    i = 0
+    j = 0
+    while i < len(lSeq) and j < len(rSeq):
+        if(lSeq[i] < rSeq[j]):
+            result.append(lSeq[i])
+            i = i + 1
+        else:
+            result.append(rSeq[j])
+            j = j + 1
+    if(i == len(lSeq)):
+        result = result + rSeq[j:]
+    else:
+        result = result + lSeq[i:]
+    return result
+
+a = merge_sort([3,4,7,1,2,9,4])
+
+b = merge_sort([2,5,9,4,5,2])
+
+
+
+
